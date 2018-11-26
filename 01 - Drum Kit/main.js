3108c60c1a1e9cb40178b23283b9db88423fab36
Jenny <zhenya.zavalova@scalefocus.com>
2018-11-26 18:52:58 +0200

    Inserting sounds using onclick event

---
+++ 01 - Drum Kit/main.js
@@ -0,0 +1,21 @@
+
+// var sounds = {
+//     65: "sounds/clap.wav",
+//     83: "sounds/hihat.wav",
+//     68: "sounds/kick.wav",
+//     70: "sounds/kick.wav",
+//     71: "sounds/boom.wav",
+//     72: "sounds/ride.wav",
+//     74: "sounds/snare.wav",
+//     75: "sounds/tom.wav",
+//     76: "sounds/tink.wav"
+//   }
+
+//   audio = new Audio("start url");
+
+//   audio.addEventListener('ended',function(){
+//         audio.src = "new url";
+//         audio.pause();
+//         audio.load();
+//         audio.play();
+//     });
