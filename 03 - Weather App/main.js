3ca72b3dc5425df5eca29af23b6568b218ea382b
Jenny <zhenya.zavalova@scalefocus.com>
2018-12-20 13:11:10 +0200

    This commit will add TONS of files and stuff in the directory, because of one 'require' variable and such

---
+++ 03 - Weather App/main.js
@@ -0,0 +1,16 @@
+"use strict";
+let request = require('request');
+
+let apiKey = '2cbaa1a99cb456e6f9ea514e5dfb9faa';
+let city = 'plovdiv';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
